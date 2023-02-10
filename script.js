cd16ee385e64ef9c9f93dc6f3818e67423719c2b
maki <komirimaha@icloud.com>
2023-02-10 22:19:54 +0900

    イベント発火　基礎編

---
+++ script.js
@@ -0,0 +1,13 @@
+const pullDownButton = document.getElementById("lists")
+
+pullDownButton.addEventListener('mouseover', function(){
+    console.log(lists);
+})
+
+pullDownButton.addEventListener('mouseout', function(){
+    console.log(lists)
+})
+
+pullDownButton.addEventListener('click', function(){
+    console.log(lists)
+})
